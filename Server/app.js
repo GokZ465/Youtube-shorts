987f70c8a84209f68bcd10721090744d3cd825be
gokul <gokul465eswaran@gmail.com>
2023-05-27 17:17:32 +0530

    Input: URL Output: number of 30 sec copies of vid

--- Server/app.js
+++ Server/app.js
@@ -17,19 +17,37 @@ app.get("/play", async (req, res) => {
   try {
     const videoId = ytdl.getURLVideoID(videoUrl);
     const videoInfo = await ytdl.getInfo(videoId);
-    const videoFormat = ytdl.chooseFormat(videoInfo.formats, {
-      quality: "highest",
-    });
+    const videoLength = parseInt(videoInfo.videoDetails.lengthSeconds);
+
+    // Generate the URLs for the 30-second clips
+    const clips = [];
+    let start = 10;
+    let end = start + 30;
+
+    while (end <= videoLength) {
+      const clipUrl = `/video?url=${encodeURIComponent(
+        videoUrl
+      )}&start=${start}&end=${end}`;
+      clips.push(clipUrl);
+
+      start += 30;
+      end = start + 30;
+    }
 
     res.send(`
       <html>
         <body>
           <h1>YouTube Video Player</h1>
-          <video controls>
-            <source src="/video?url=${encodeURIComponent(
-              videoUrl
-            )}" type="video/mp4">
-          </video>
+          ${clips
+            .map(
+              (clipUrl) => `
+              <video controls>
+                <source src="${clipUrl}" type="video/mp4">
+              </video>
+              <br>
+            `
+            )
+            .join("")}
         </body>
       </html>
     `);
@@ -37,9 +55,13 @@ app.get("/play", async (req, res) => {
     res.status(500).json({ error: "Failed to load video" });
   }
 });
+
+
 app.get("/video", async (req, res) => {
   const videoUrl = req.query.url;
-
+  const start = parseInt(req.query.start);
+  const end = parseInt(req.query.end);
+  //   const duration = end - start;
   if (!videoUrl) {
     return res.status(400).json({ error: "Video URL is required" });
   }
@@ -61,7 +83,8 @@ app.get("/video", async (req, res) => {
     // Create a new ffmpeg command
     const command = ffmpeg(videoStream)
       .format("mp4")
-      .outputOptions("-t", "30") // Set the duration of the cropped video (30 seconds)
+      .setStartTime(start)
+      .setDuration(end - start) // Set the duration of the cropped video (30 seconds)
       .on("error", (err) => {
         console.error("Error during video cropping:", err);
         res.status(500).json({ error: "Failed to crop video" });
--- src/VideoDownloader/VideoDownloader.jsx
+++ src/VideoDownloader/VideoDownloader.jsx
@@ -8,9 +8,19 @@ const VideoDownloader = () => {
   };
 
   const handleDownload = () => {
-    const apiUrl = `http://localhost:3000/play?url=${encodeURIComponent(
+    const numClips = 2; // Number of clips you want to generate
+    const clipDuration = 30; // Duration of each clip in seconds
+
+    let apiUrl = `http://localhost:3000/play?url=${encodeURIComponent(
       videoUrl
-    )}`;
+    )}&clips=${numClips}`;
+
+    for (let i = 0; i < numClips; i++) {
+      const start = i * clipDuration;
+      const end = (i + 1) * clipDuration;
+      apiUrl += `&start${i + 1}=${start}&end${i + 1}=${end}`;
+    }
+
     window.open(apiUrl, "_blank");
   };
 
